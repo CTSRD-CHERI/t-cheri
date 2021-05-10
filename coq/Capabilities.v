eb8d87f329b1f67e30af49c962b454508128e03a
Vadim Zaliva <lord@crocodile.org>
2021-05-10 15:53:17 -0700

    support for empty address range

--- coq/Capabilities.v
+++ coq/Capabilities.v
@@ -45,17 +45,6 @@ Class Address (A:Type) :=
      end of interval (exclusive), not the lenght! No error handling
      for now. We will return empty set instead. But may be later
      extended with error handling.
-
-     TODO: @thomas.sewell: Here's one of the headaches: a 0-length
-     capability may be valid in CHERI. It isn't capable of enabling
-     any operation at any address, but it might be passed around by
-     software as a token. Maybe. The model apparently supports this
-     but, to my knowledge, nobody actually does this yet, so we don't
-     know if there are any extra corner cases in the
-     implementation. Anyway, you'll note that the inclusive encoding
-     of such a capability is strange, and creates potentially
-     dangerous confusion between the 0-length capability starting at
-     address 0 and the 2^64-length capability starting at address 0.
    *)
   address_range: A -> A -> {l:list A| NoDup l} ;
   }.
@@ -79,19 +68,19 @@ Class Permission (P:Type)
       `{ARCH: Arch A}:=
   {
   (* Convenience functions to examine some permission bits *)
-  permits_execute : P -> bool;
-  permits_ccall : P -> bool;
-  permits_load : P -> bool;
-  permits_load_cap : P -> bool;
-  permits_seal : P -> bool;
-  permits_store : P -> bool;
-  permits_store_cap : P -> bool;
-  permits_store_local_cap : P -> bool;
-  permits_system_access : P -> bool;
-  permits_unseal : P -> bool;
+  permits_execute: P -> bool;
+  permits_ccall: P -> bool;
+  permits_load: P -> bool;
+  permits_load_cap: P -> bool;
+  permits_seal: P -> bool;
+  permits_store: P -> bool;
+  permits_store_cap: P -> bool;
+  permits_store_local_cap: P -> bool;
+  permits_system_access: P -> bool;
+  permits_unseal: P -> bool;
 
   (* Get all permission bits *)
-  get_bits : P -> word (perms_nbits)
+  get_bits: P -> word (perms_nbits)
   }.
 
 Class Capability (C:Type)
@@ -99,20 +88,27 @@ Class Capability (C:Type)
       `{ADR: Address address}
       `{PERM: @Permission P A ARCH} :=
   {
-  is_tagged : C -> bool;
-  is_sealed : C -> bool;
-  is_sentry : C -> bool;
-  is_indirect_sentry : C -> bool;
-  get_base : C -> address;
-  get_top : C -> address;
-  get_obj_type : C -> word (otype_nbits);
-  get_perms : C -> P;
-  get_cursor : C -> address;
-  is_global : C -> bool;
-  seal : C -> word (otype_nbits) -> C;
-  unseal : C -> C;
-  clear_global : C -> C;
-  cap_of_mem_bytes : (Vector.t memory_byte capability_nbyes) -> bool -> option C;
+
+  is_tagged: C -> bool;
+  is_sealed: C -> bool;
+  is_sentry: C -> bool;
+  is_indirect_sentry: C -> bool;
+
+  (* Returns either inclusive bounds for covered
+     memory region, or None if empty *)
+  get_bounds: C -> option (address*address);
+
+  get_obj_type: C -> word (otype_nbits);
+  get_perms: C -> P;
+  get_cursor: C -> address;
+
+  seal: C -> word (otype_nbits) -> C;
+  unseal: C -> C;
+
+  is_global: C -> bool;
+  clear_global: C -> C;
+
+  cap_of_mem_bytes: (Vector.t memory_byte capability_nbyes) -> bool -> option C;
   }.
 
 
@@ -123,6 +119,9 @@ Section CapabilityProperties.
   Context `{CAPA: @Capability C _ ARCH  _ ADR _ PERM}.
 
   Definition get_mem_region `{Capability C} (c:C): address_set
-    := address_range (get_base c) (get_top c).
+    := match get_bounds c with
+       | Some (from,to) => address_range from to
+       | None => empty_address_set
+       end.
 
 End CapabilityProperties.
