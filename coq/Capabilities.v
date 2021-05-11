caa6c00a8462e2e789b86f2a5f44309d6aa72263
Vadim Zaliva <lord@crocodile.org>
2021-05-11 15:32:16 -0700

    empty intervals with base

--- coq/Capabilities.v
+++ coq/Capabilities.v
@@ -3,6 +3,7 @@
 Require Import Coq.Lists.List.
 Require Import Coq.Vectors.Vector.
 Require Import Coq.Arith.PeanoNat.
+Require Import Coq.Relations.Relation_Definitions.
 
 Require Import bbv.Word.
 
@@ -33,26 +34,65 @@ Class Arch (A:Type) :=
   memory_byte:Type;
   }.
 
+Section Interval.
+  Variable V:Type.
+  Variable V_lt: V -> V -> Prop.
+  Variable V_lt_irref: forall a, ~ V_lt a a.
+
+  Local Notation "x < y"  := (V_lt x y).
+  Local Notation "x <= y" := (V_lt x y /\ x=y).
+
+  Inductive Interval :=
+  | Incl_Interval (base top:V): (base=top /\ base < top) -> Interval
+  | Empty_Interval (base:V): Interval.
+
+  (* [<=] relation on intervals *)
+  Definition interval_leq: relation (Interval) :=
+    fun i1 i2 =>
+      match i1, i2 with
+      | Empty_Interval b1, Empty_Interval b2 => b1 = b2
+      | @Incl_Interval b1 t1 _, @Incl_Interval b2 t2 _ =>  b2 <= b1 /\ t1 <= t2
+      | Empty_Interval b1, @Incl_Interval b2 t2 _ =>  b2 <= b1 /\ b1 <= t2
+      | @Incl_Interval b1 t1 _, Empty_Interval b2 =>  False
+      end.
+
+  Fact interval_leq_dec:
+    (forall x y : V, {x = y} + {x <> y}) ->
+    (forall x y, {V_lt x y}+{~ V_lt x y}) ->
+    forall a b, {interval_leq a b}+{~ interval_leq a b}.
+  Proof.
+    intros EQDEC LTDEC a b.
+    destruct a as [b1 t1 [b1t1e b1t1l] | b1], b as [b2 t2 [b2t2e b2t2l] | b2]; subst;cbn;auto.
+    -
+      apply V_lt_irref in b1t1l.
+      tauto.
+    -
+      apply V_lt_irref in b2t2l.
+      tauto.
+  Qed.
+
+End Interval.
+Arguments Incl_Interval {V}%type_scope {V_lt}%type_scope.
+Arguments Empty_Interval {V}%type_scope {V_lt}%type_scope.
+
 Class Address (A:Type) :=
   {
-
   (* "less than" *)
-  address_le: A -> A -> Prop ;
+  address_lt: relation A;
+  address_lt_irref: forall a, ~ address_lt a a;
 
   address_eq_dec: forall a b : A, {a = b} + {a <> b};
-  address_le_dec: forall a b : A, {address_le a b}+{~ address_le a b};
+  address_lt_dec: forall a b : A, {address_lt a b}+{~ address_lt a b};
 
-  (* Generates of set of addresses in the given range. NOTE: unlike
-     functoin with same name in the `capablities.lem`, the 2nd
-     argument is end of interval (inclusve), not the lenght! *)
-  address_range: A -> A -> {l:list A| NoDup l} ;
+  (* Generates of set of addresses in the given range. *)
+  addresses_in_interval: (Interval address_lt)-> {l:list A| NoDup l} ;
   }.
 
 Section AddressHelpers.
   Context `{ADR: Address A}.
 
-  (* inclusive address interval. Could not be empty. *)
-  Definition address_interval := {'(a,b) | address_le a b}.
+  (* address interval. *)
+  Definition address_interval := Interval address_lt.
 
   (* Set of addresses type aliase *)
   Definition address_set := {l:list A| NoDup l} .
@@ -64,12 +104,13 @@ Section AddressHelpers.
   Definition address_eqb: A -> A -> bool :=
     fun a b => if address_eq_dec a b then true else false.
 
-  (* boolean versoin of [address_le] preficate *)
-  Definition address_leb: A -> A -> bool :=
-    fun a b => if address_le_dec a b then true else false.
+  (* boolean versoin of [address_lt] preficate *)
+  Definition address_ltb: A -> A -> bool :=
+    fun a b => if address_lt_dec a b then true else false.
 
-  Definition address_leqb: A -> A -> bool :=
-    fun a b => address_leb a b || address_eqb a b.
+  (* boolean "less of equal" perdicate on on addresses *)
+  Definition address_leb: A -> A -> bool :=
+    fun a b => address_ltb a b || address_eqb a b.
 
 End AddressHelpers.
 
@@ -104,8 +145,8 @@ Class Capability (C:Type)
   is_indirect_sentry: C -> bool;
 
   (* Returns either inclusive bounds for covered
-     memory region, or None if empty *)
-  get_bounds: C -> option address_interval;
+     memory region *)
+  get_bounds: C -> address_interval;
 
   get_obj_type: C -> word (otype_nbits);
   get_perms: C -> P;
@@ -120,7 +161,6 @@ Class Capability (C:Type)
   cap_of_mem_bytes: (Vector.t memory_byte capability_nbyes) -> bool -> option C;
   }.
 
-
 Section CapabilityProperties.
 
   Context `{ARCH: Arch A}
@@ -129,17 +169,14 @@ Section CapabilityProperties.
           `{CAPA: @Capability C A ARCH  address ADR P PERM}.
 
   Definition get_mem_region (c:C): address_set
-    := match get_bounds c with
-       | Some (exist _ (from,to) _) => address_range from to
-       | None => empty_address_set
-       end.
+    := addresses_in_interval (get_bounds c).
 
   Definition leq_bounds (c1 c2:C): bool :=
-    match get_bounds c1, get_bounds c2 with
-    | Some (exist _ (b1,t1) _), Some (exist _ (b2, t2) _) =>
-      address_leb b2 b1 && address_leb t1 t2
-    | None, None => true
-    | _,_ => false
-    end.
+    if interval_leq_dec
+         address_lt_irref
+         address_eq_dec
+         address_lt_dec
+         (get_bounds c1) (get_bounds c2)
+    then true else false.
 
 End CapabilityProperties.
