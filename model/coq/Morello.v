a9150ecbe9bedbdd78a4e52fd4389b44cbd39043
Vadim Zaliva <lord@crocodile.org>
2021-05-20 10:47:46 -0700

    CPermission(MPermission) instance

--- model/coq/Morello.v
+++ model/coq/Morello.v
@@ -34,6 +34,32 @@ Record MPermission :=
   (* TODO: User[N] *)
   }.
 
+
+Instance CPermissoin_MPermission:
+  CPermission(MPermission) :=
+  {|
+  Capabilities.permits_execute :=
+    fun p : MPermission => permits_execute p = true;
+  Capabilities.permits_ccall :=
+    fun p : MPermission => permits_ccall p = true;
+  Capabilities.permits_load :=
+    fun p : MPermission => permits_load p = true;
+  Capabilities.permits_load_cap :=
+    fun p : MPermission => permits_load_cap p = true;
+  Capabilities.permits_seal :=
+    fun p : MPermission => permits_seal p = true;
+  Capabilities.permits_store :=
+    fun p : MPermission => permits_store p = true;
+  Capabilities.permits_store_cap :=
+    fun p : MPermission => permits_store_cap p = true;
+  Capabilities.permits_store_local_cap :=
+    fun p : MPermission => permits_store_local_cap p = true;
+  Capabilities.permits_system_access :=
+    fun p : MPermission => permits_system_access p = true;
+  Capabilities.permits_unseal :=
+    fun p : MPermission => permits_unseal p = true
+  |}.
+
 Record MCapability :=
   {
   is_valid: bool;
